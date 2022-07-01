0da7d9b09c2ef421cf5908cb9585458b600bb24b
kilgore92 <ishaan.bits92@gmail.com>
2022-07-01 12:58:22 +0200

    Save image, label, and prediction using the correct meta-data. Confirmed visually using MeVisLab

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -16,13 +16,17 @@ def create_data_dicts(patient_dir_list=None):
     data_dicts = []
 
     for p_dir in patient_dir_list:
+        p_id = p_dir.split(os.sep)[-1]
         scan_dirs  = [f.path for f in os.scandir(p_dir) if f.is_dir()]
         for s_dir in scan_dirs:
+            s_id = s_dir.split(os.sep)[-1]
             data_dict = {}
             data_dict['image'] = []
             for chidx in range(6):
                 data_dict['image'].append(os.path.join(s_dir, 'DCE_channel_{}.nii'.format(chidx)))
             data_dict['label'] = os.path.join(s_dir, 'LiverMask.nii')
+            data_dict['patient_id'] = p_id
+            data_dict['scan_id'] = s_id
             data_dicts.append(data_dict)
 
     return data_dicts
---
+++ src/test.py
@@ -0,0 +1,133 @@
+"""
+
+Script to evaluate liver segmentation using 3-D UNet
+
+@author: Ishaan Bhat
+@email: ishaan@isi.uu.nl
+
+"""
+from monai.networks.nets import UNet
+from monai.metrics import DiceMetric
+from monai.handlers.utils import from_engine
+from datapipeline import *
+from monai.inferers import sliding_window_inference
+from monai.config import print_config
+from monai.apps import download_and_extract
+import torch
+import torch.nn as nn
+from argparse import ArgumentParser
+import shutil
+from helper_functions import *
+from utils.utils import *
+
+
+def test(args):
+
+    # Set device
+    device = torch.device('cuda:{}'.format(args.gpu_id))
+
+
+    # Define model
+
+    model = UNet(spatial_dims=3,
+                 in_channels=6,
+                 out_channels=1,
+                 channels=(64, 128, 256, 512),
+                 strides=(2, 2, 2),
+                 num_res_units=1)
+
+    # Load model
+
+    load_dict = load_model(model=model,
+                           checkpoint_dir=args.checkpoint_dir,
+                           training=False)
+
+    model = load_dict['model']
+    model.eval()
+    model.to(device)
+
+    test_patients = joblib.load('test_patients.pkl')
+    test_dicts = create_data_dicts(test_patients)
+
+    test_loader, transforms = create_dataloader(data_dicts=test_dicts,
+                                                train=False,
+                                                batch_size=1)
+
+    dice_metric = DiceMetric(include_background=False, reduction="mean")
+
+    post_transforms = Compose([EnsureTyped(keys=['pred']),
+                               Invertd(keys=['pred', 'label', 'image'],
+                                       transform=transforms,
+                                       orig_keys='image',
+                                       meta_keys=['pred_meta_dict', 'label_meta_dict', 'image_meta_dict'],
+                                       nearest_interp=False,
+                                       to_tensor=True),
+                               Activationsd(keys='pred', sigmoid=True),
+                               # After resampling some label voxels can get float values
+                               AsDiscreted(keys=['pred', 'label'], threshold=0.5),
+                               KeepLargestConnectedComponentd(keys='pred'),
+                               FillHolesd(keys='pred')])
+    with torch.no_grad():
+        for test_data in test_loader:
+            # Idx 0 => Assumes batch_size = 1
+            patient_id = test_data['patient_id'][0]
+            scan_id = test_data['scan_id'][0]
+            test_inputs, test_labels = (test_data['image'].to(device),
+                                      test_data['label'].to(device))
+
+            roi_size = (128, 128, 48)
+            sw_batch_size = 4
+
+            test_data['pred'] = sliding_window_inference(test_inputs,
+                                                         roi_size,
+                                                         sw_batch_size,
+                                                         model,
+                                                         overlap=0.5)
+
+            test_data = [post_transforms(i) for i in decollate_batch(test_data)]
+
+
+            test_outputs, test_labels = from_engine(["pred", "label"])(test_data)
+            # Dice metric for current iteration
+            dice_metric(y_pred=test_outputs, y=test_labels)
+            print('Dice metric: {}'.format(dice_metric.aggregate().item()))
+
+            # Save the image, label, and prediction (in the original spacing)
+            save_dir = os.path.join(args.checkpoint_dir, 'results', patient_id, scan_id)
+            if os.path.exists(save_dir) is False:
+                os.makedirs(save_dir)
+
+            save_op_image = SaveImaged(keys='image',
+                                       output_postfix='image',
+                                       output_dir=save_dir,
+                                       separate_folder=False)
+
+            save_op_label = SaveImaged(keys='label',
+                                       output_postfix='label',
+                                       output_dir=save_dir,
+                                       separate_folder=False)
+
+            save_op_pred = SaveImaged(keys='pred',
+                                       output_postfix='pred',
+                                       output_dir=save_dir,
+                                       separate_folder=False)
+
+            for test_dict in test_data:
+                save_op_image(test_dict)
+                save_op_label(test_dict)
+                save_op_pred(test_dict)
+
+            dice_metric.reset()
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('--checkpoint_dir', type=str, required=True)
+    parser.add_argument('--gpu_id', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=2)
+
+    args = parser.parse_args()
+
+    test(args)
+
+
