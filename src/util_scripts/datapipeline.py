f1462205ac0716318db942cd7b2997bd5c6dc676
kilgore92 <ishaan.bits92@gmail.com>
2022-08-01 11:00:32 +0200

    Modify MONAI transform pipeline to get rid of all data augmentation transformations

--- src/util_scripts/datapipeline.py
+++ src/util_scripts/datapipeline.py
@@ -13,7 +13,74 @@ from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
 import joblib
 from helper_functions import create_data_dicts
 
-def create_dataloader(data_dicts=None, train=True, batch_size=4):
+def create_dataloader_lesion_matching(data_dicts=None, train=True, batch_size=4):
+
+    if train is True:
+        transforms = Compose([LoadImaged(keys=["image", "label"]),
+
+                              # Add fake channel to the label
+                              AddChanneld(keys=["label"]),
+
+                              # Make sure image is channel first
+                              EnsureChannelFirstd(keys=["image"]),
+
+                              Orientationd(keys=["image", "label"], axcodes="RAS"),
+
+                              # Isotropic spacing
+                              Spacingd(keys=["image", "label"],
+                                       pixdim=(1.543, 1.543, 1.543),
+                                       mode=("bilinear", "nearest")),
+
+                              # Extract 128x128x48 3-D patches
+                              RandSpatialCropd(keys=["image", "label"],
+                                               roi_size=[128, 128, 48],
+                                               random_size=False),
+
+                              NormalizeIntensityd(keys=["image"],
+                                                  nonzero=True,
+
+
+                              EnsureTyped(keys=["image", "label"])
+                              ])
+
+    else:
+        transforms = Compose([LoadImaged(keys=["image", "label"]),
+
+                              # Add fake channel to the label
+                              AddChanneld(keys=["label"]),
+
+                              EnsureChannelFirstd(keys=["image"]),
+
+                              Orientationd(keys=["image", "label"], axcodes="RAS"),
+
+                              Spacingd(keys=["image", "label"],
+                                       pixdim=(1.543, 1.543, 1.543),
+                                       mode=("bilinear", "nearest")),
+
+                              NormalizeIntensityd(keys=["image"],
+                                                  nonzero=True,
+                                                  channel_wise=True),
+
+                              EnsureTyped(keys=["image", "label"])
+                              ])
+
+    ds = CacheDataset(data=data_dicts,
+                      transform=transforms,
+                      cache_rate=1.0,
+                      num_workers=4)
+
+    loader = DataLoader(ds,
+                        batch_size=batch_size,
+                        shuffle=train,
+                        num_workers=4)
+
+    if train is True:
+        return loader
+    else:
+        return loader, transforms
+
+
+def create_dataloader_liver_seg(data_dicts=None, train=True, batch_size=4):
 
     if train is True:
         transforms = Compose([LoadImaged(keys=["image", "label"]),
@@ -89,7 +156,7 @@ def create_dataloader(data_dicts=None, train=True, batch_size=4):
     else:
         return loader, transforms
 
-def debug_dataloader(data_dicts=None, batch_size=4):
+def debug_dataloader_liver_seg(data_dicts=None, batch_size=4):
     """
     Create a bare-bones transform pipeline to flush out bugs in the pipeline
 
