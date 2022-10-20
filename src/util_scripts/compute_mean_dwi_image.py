f40133411237f4634686011cad7fa11b9fd86cc1
kilgore92 <ishaan.bits92@gmail.com>
2022-10-20 15:57:13 +0200

    Add script to create mean DWI image that can be used for paired 3D image registration

---
+++ src/util_scripts/compute_mean_dwi_image.py
@@ -0,0 +1,58 @@
+""""
+
+Script to compute the mean DWI MR image for a patient
+
+@author: Ishaan Bhat
+@email: ishaan@isi.uu.nl
+
+"""
+
+
+DATA_DIR = '/home/ishaan/UMC_Data/follow_up_scans'
+
+import os
+import numpy as np
+import SimpleITK as sitk
+
+def compute_mean_dwi_image(img, label):
+
+    assert(isinstance(img, sitk.Image))
+    assert(isinstance(label, sitk.Image))
+
+    img_np = sitk.GetArrayFromImage(img)
+
+    # Compute mean over b-values
+    img_np_mean = np.mean(img_np, axis=0)
+
+    img_itk_mean = sitk.GetImageFromArray(img_np_mean)
+
+    # Populate metadata fields
+    img_itk_mean.SetOrigin(label.GetOrigin())
+    img_itk_mean.SetDirection(label.GetDirection())
+    img_itk_mean.SetSpacing(label.GetSpacing())
+
+    return img_itk_mean
+
+
+if __name__ == '__main__':
+
+    pat_dirs = [f.path for f in os.scandir(DATA_DIR) if f.is_dir()]
+
+    for p_dir in pat_dirs:
+        scan_dirs  = [f.path for f in os.scandir(p_dir) if f.is_dir()]
+        for s_dir in scan_dirs:
+            print('Processing: {}'.format(s_dir))
+            try:
+                dwi_img = sitk.ReadImage(os.path.join(s_dir, 'DWI_reg.nii'))
+            except RuntimeError:
+                print('DWI image not found in {}'.format(s_dir))
+                continue
+
+            label = sitk.ReadImage(os.path.join(s_dir, 'LiverMask.nii'))
+            mean_dwi_img = compute_mean_dwi_image(img=dwi_img,
+                                                  label=label)
+            sitk.WriteImage(mean_dwi_img,
+                            os.path.join(s_dir, 'DWI_reg_mean.nii'))
+
+
+
