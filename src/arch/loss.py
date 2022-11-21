1. Fix minor bug in train where the value for K was different for model and loss function 2. Weight calculation for the weightedCE loss reflects the equation in the Grewal et al. (2020) SPIE paper 3. Convert image coords to int when rescaling the grid

src/arch/loss.py
@@ -102,17 +102,22 @@ def custom_loss(landmark_logits1, landmark_logits2, desc_pairs_score, desc_pairs
 
     # Descriptor loss: Weighted CE
     b, k1, k2 = match_target.shape
-    wt = float(k) / float(k)** 2
+    Npos = match_target.sum()
+    Nneg = b*k1*k2 - Npos
+
+    pos_weight = Nneg/(Npos+Nneg)
+    neg_weight = Npos/(Npos+Nneg)
+
     desc_loss1 = F.cross_entropy(desc_pairs_score,
                                  match_target.long().view(-1),
-                                 weight=torch.tensor([wt, 1 - wt]).to(device))
+                                 weight=torch.tensor([neg_weight, pos_weight]).to(device))
 
     # Descriptor loss: Contrastive loss (with margin)
     # TODO: Margin (pos and neg should be hyper-parameters)
-    Npos = match_target.sum()
-    Nneg = b*k1*k2 - Npos
-    pos_loss = torch.sum(match_target * torch.max(torch.zeros_like(desc_pairs_norm).to(device), desc_pairs_norm - 0.1)) / (2*Npos + 1e-6)
-    neg_loss = torch.sum((1.0 - match_target) * torch.max(torch.zeros_like(desc_pairs_norm).to(device), 1.0 - desc_pairs_norm)) / (2*Nneg + 1e-6)
+    mpos = 0.1
+    mneg = 1
+    pos_loss = torch.sum(match_target * torch.max(torch.zeros_like(desc_pairs_norm).to(device), desc_pairs_norm - mpos)) / (2*Npos + 1e-6)
+    neg_loss = torch.sum((1.0 - match_target) * torch.max(torch.zeros_like(desc_pairs_norm).to(device), mneg - desc_pairs_norm)) / (2*Nneg + 1e-6)
     desc_loss2 = pos_loss + neg_loss
     desc_loss = desc_loss1 + desc_loss2
 

src/arch/model.py
@@ -206,7 +206,7 @@ class LesionMatchingModel(nn.Module):
         pts = (pts + 1.)/2.
 
         # Scale to image dimensions (DHW ordering)
-        pts = pts * torch.Tensor([(shape[0]-1, shape[1]-1, shape[2]-1)]).view(1, 1, 3).to(pts.device)
+        pts = pts * torch.Tensor([(shape[0]-1, shape[1]-1, shape[2]-1)]).view(1, 1, 3).int().to(pts.device)
 
         return pts
 
@@ -293,12 +293,12 @@ class LesionMatchingModel(nn.Module):
 
 
             item_kpts = torch.zeros(size=(N, 4),
-                                    dtype=kpt_map.dtype).to(kpt_map.device)
+                                    dtype=kpt_probmap.dtype).to(kpt_map.device)
 
             item_kpts[:, 0] = xs
             item_kpts[:, 1] = ys
             item_kpts[:, 2] = zs
-            item_kpts[:, 3] = kpt_probmap_suppressed[batch_idx, zs, ys, xs]
+            item_kpts[:, 3] = kpt_probmap_suppressed[batch_idx, zs , ys, xs]
 
             idxs_desc = torch.argsort(-1*item_kpts[:, 3])
 

src/test.py
@@ -30,20 +30,6 @@ import random
 
 ROI_SIZE = (96, 96, 48)
 
-# TODO: Add ITK metadata (spacing, direction, origin)
-def convert_kpt_map_to_itk(kpt_map):
-
-    assert(isinstance(kpt_map, torch.Tensor))
-
-    # Get rid of "channel" axis
-    kpt_map = torch.squeeze(kpt_map, dim=0)
-
-    kpt_map_np = kpt_map.cpu().numpy()
-
-    kpt_map_np = np.transpose(kpt_map_np, (2, 0, 1)) # Z-first for ITK conversion
-    kpt_map_itk = sitk.GetImageFromArray(kpt_map_np)
-    return kpt_map_itk
-
 
 def test(args):
 
@@ -159,16 +145,6 @@ def test(args):
                                                                         predictor=model.get_patch_keypoint_scores,
                                                                         overlap=0.5)
 
-
-                # Mask using liver mask
-                # Assign a large negative value to logit values corr. to voxels outside the liver
-                # => When the sigmoid scales the logits to range [0, 1], the values outside the liver
-                # have probability ~ 0 of being sampled
-#                mask_tensor = -1*1e10*torch.ones_like(kpts_logits_1)
-#                kpts_logits_1 = torch.where(liver_mask.to(kpts_logits_1.device) == 1, kpts_logits_1, mask_tensor)
-#                kpts_logits_2 = torch.where(liver_mask.to(kpts_logits_2.device) == 1, kpts_logits_2, mask_tensor)
-
-
                 # Feature maps
                 features_1_low, features_1_high, features_2_low, features_2_high =\
                                                         sliding_window_inference(inputs=images_cat.to(device),
@@ -190,7 +166,7 @@ def test(args):
                                           features_1=features_1,
                                           features_2=features_2,
                                           conf_thresh=0.1,
-                                          num_pts=1024,
+                                          num_pts=256,
                                           mask=liver_mask.to(device),
                                           mask2=liver_mask_hat.to(device))
 