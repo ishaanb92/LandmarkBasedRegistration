38120cb2f9dac9167125bbb06b918d85b5fc2c75
kilgore92 <ishaan.bits92@gmail.com>
2023-02-21 12:09:25 +0100

    Make command line arguments simpler for TRE box-plot script

--- analysis/plot_registration_metrics.py
+++ analysis/plot_registration_metrics.py
@@ -18,17 +18,18 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-    parser.add_argument('--result_dirs', type=str, help='Registration configurations we want to compare' ,
+    parser.add_argument('--result_dir', type=str, help='Directory where all results are stored')
+    parser.add_argument('--folders', type=str, help='Registration configurations we want to compare' ,
                     nargs='+')
     parser.add_argument('--legends', type=str, help='Legends for registration comparisons', nargs='+')
-    parser.add_argument('--output_file', type=str, help='Name of output file', default='/home/ishaan/paper4/elastix_results/comparison.png')
+    parser.add_argument('--output_file', type=str, help='Name of output file', default='comparison.png')
 
     args = parser.parse_args()
 
-    assert(isinstance(args.result_dirs, list))
+    assert(isinstance(args.folders, list))
     assert(isinstance(args.legends, list))
 
-    baseline_dir = args.result_dirs[0]
+    baseline_dir = os.path.join(args.result_dir, args.folders[0])
 
     # Extract patient IDs from baseline directory
     pids = [f.path.split(os.sep)[-1] for f in os.scandir(baseline_dir) if f.is_dir()]
@@ -46,9 +47,9 @@ if __name__ == '__main__':
         tre_dict['Registration type'].extend(['Pre-registration' for i in range(pre_reg_tre.shape[0])])
         tre_dict['TRE (mm)'].extend(list(pre_reg_tre))
         # Loop over registration configurations
-        for idx, rdir in enumerate(args.result_dirs):
-            pdir = os.path.join(rdir, pid)
-            post_reg_tre = np.load(os.path.join(rdir, pid, 'post_reg_error.npy'))
+        for idx, rdir in enumerate(args.folders):
+            pdir = os.path.join(args.result_dir, rdir, pid)
+            post_reg_tre = np.load(os.path.join(pdir, 'post_reg_error.npy'))
             tre_dict['Patient ID'].extend([pid for i in range(pre_reg_tre.shape[0])])
             tre_dict['Registration type'].extend([args.legends[idx] for i in range(post_reg_tre.shape[0])])
             tre_dict['TRE (mm)'].extend(list(post_reg_tre))
@@ -66,7 +67,7 @@ if __name__ == '__main__':
                 hue='Registration type',
                 ax=ax)
 
-    fig.savefig(args.output_file,
+    fig.savefig(os.path.join(args.result_dir, args.output_file),
                 bbox_inches='tight')
 
 
