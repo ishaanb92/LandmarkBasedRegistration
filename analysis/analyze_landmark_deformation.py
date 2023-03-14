e52ac310ba08e45e24cd97c115f9e7568e2c77ac
kilgore92 <ishaan.bits92@gmail.com>
2023-03-14 16:41:05 +0100

    Calculate jacobian and jacobian determinant of the transformation defined by point pairs, constructed using thin-plate splines

--- analysis/analyze_landmark_deformation.py
+++ analysis/analyze_landmark_deformation.py
@@ -12,7 +12,8 @@ import SimpleITK as sitk
 import numpy as np
 from argparse import ArgumentParser
 from lesionmatching.util_scripts.utils import *
-from lesionmatching.data.deformations import construct_tps_defromation
+from lesionmatching.util_scripts.image_utils import *
+from lesionmatching.data.deformations import construct_tps_defromation, calculate_jacobian_determinant
 
 if __name__ == '__main__':
 
@@ -59,15 +60,27 @@ if __name__ == '__main__':
         print('Fitting TPS for landmark correspondences for Patient {}'.format(pid))
 
         # 1. Fit thin-plate spline to define DVF based on point correspondences
+        # Smoothing term is set to 0 for now, we want exact interpolation to study the
+        # properties of the deformations defined by the (predicted/GT) landmark correspondences
         T = construct_tps_defromation(p1=fixed_points_scaled,
                                       p2=moving_points_scaled,
                                       shape=np.array(shape))
 
-        print(T.shape)
+        # 2. Compute determinant of Jacobian
+        jac_det = calculate_jacobian_determinant(deformed_grid=T)
 
-        # TODO
-        # 2. Construct Jacobian of T
+        print('Jacobian determinant :: Min = {}, Max = {}'.format(np.amin(jac_det),
+                                                                  np.amax(jac_det)))
+        # 3. Save the jac_det as an ITK image
+        save_ras_as_itk(img=jac_det,
+                        metadata={'spacing':fixed_image_itk.GetSpacing(),
+                                  'origin':fixed_image_itk.GetOrigin(),
+                                  'direction':fixed_image_itk.GetDirection()},
+                        fname=os.path.join(pdir, 'jac_det.mha'))
 
-        # 3. Compute determinant of Jacobian
+
+
+
+
 
 
