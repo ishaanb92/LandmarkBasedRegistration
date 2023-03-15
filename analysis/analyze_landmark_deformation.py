1. Remove 'neighbours' parameter from TPS function (this vastly improves Jacobian det. values) 2. Used scaled grid while resampling image (minor bugfix)

analysis/analyze_landmark_deformation.py
@@ -46,6 +46,7 @@ if __name__ == '__main__':
         fixed_image_shape = fixed_image_itk.GetSize()
         moving_image_shape = moving_image_itk.GetSize()
 
+        # Landmarks in fixed and moving images are specified in world coordinates
         if args.mode == 'nn':
             fixed_points_arr = parse_points_file(os.path.join(pdir,
                                                               'fixed_landmarks_elx.txt'))
@@ -63,8 +64,17 @@ if __name__ == '__main__':
         else:
             raise RuntimeError('{} is not a valid option for mode'.format(args.mode))
 
-        # Scale the coordinates between [0, 1]
 
+        # Convert world coordinates to voxel coordinates
+        fixed_points_arr = map_world_coord_to_voxel_index(world_coords=fixed_points_arr,
+                                                          spacing=fixed_image_itk.GetSpacing(),
+                                                          origin=fixed_image_itk.GetOrigin())
+
+        moving_points_arr = map_world_coord_to_voxel_index(world_coords=moving_points_arr,
+                                                           spacing=moving_image_itk.GetSpacing(),
+                                                           origin=moving_image_itk.GetOrigin())
+
+        # Scale the coordinates between [0, 1]
         fixed_points_scaled = np.divide(fixed_points_arr,
                                         np.expand_dims(np.array(fixed_image_shape),
                                                        axis=0))

lesionmatching/data/deformations.py
@@ -212,8 +212,7 @@ def construct_tps_defromation(p1=None,
                                        d=p2,
                                        smoothing=0.0,
                                        kernel='thin_plate_spline',
-                                       degree=1,
-                                       neighbors=10)
+                                       degree=1)
 
     # Shape: [3, X, Y, Z]
     grid = np.array(np.meshgrid(np.linspace(0, 1, shape[0]),
@@ -308,10 +307,10 @@ def resample_image(image:np.ndarray,
     scaled_transformed_coordinates[2, ...] = image.shape[2]*transformed_coordinates[2, ...]
 
     resampled_image = map_coordinates(input=image,
-                                      coordinates=transformed_coordinates,
+                                      coordinates=scaled_transformed_coordinates,
                                       order=3,
                                       mode='constant',
-                                      cval=0)
+                                      cval=np.amin(image))
 
     return resampled_image
 