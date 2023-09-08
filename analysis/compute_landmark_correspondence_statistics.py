5de1181082311d25f95d1b83e668a2f7446aca22
kilgore92 <ishaan.bits92@gmail.com>
2023-09-08 15:05:24 +0200

    While computing landmark statistics, handle landmark correspondences found outside the image domain (this can happen because we often pad during patch creation to maintain uniform patch-size and ensure full coverage)

--- analysis/compute_landmark_correspondence_statistics.py
+++ analysis/compute_landmark_correspondence_statistics.py
@@ -36,6 +36,8 @@ if __name__ == '__main__':
 
     for idx, pdir in enumerate(pdirs):
 
+        error_found = False
+
         pid = pdir.split(os.sep)[-1]
 
         pdata_dir = os.path.join(DATA_DIR, pid)
@@ -58,7 +60,10 @@ if __name__ == '__main__':
                                                                       'moving_landmarks_elx.txt'))
 
         assert(fixed_image_landmarks.shape[0] == moving_image_landmarks.shape[0])
+
         n_landmarks[idx] = fixed_image_landmarks.shape[0]
+
+
 
         # 3. Convert physical coordinates into voxel indices
         fixed_image_landmarks_voxel = map_world_coord_to_voxel_index(world_coords=fixed_image_landmarks,
@@ -68,6 +73,69 @@ if __name__ == '__main__':
         moving_image_landmarks_voxel = map_world_coord_to_voxel_index(world_coords=moving_image_landmarks,
                                                                       spacing=moving_mask_itk.GetSpacing(),
                                                                       origin=moving_mask_itk.GetOrigin()).astype(np.int32)
+
+        max_landmark_coordinates_fixed = np.max(fixed_image_landmarks_voxel, axis=0)
+        max_landmark_coordinates_moving = np.max(moving_image_landmarks_voxel, axis=0)
+
+        if max_landmark_coordinates_fixed[0] > fixed_mask_np.shape[0] \
+                or max_landmark_coordinates_fixed[1] > fixed_mask_np.shape[1] \
+                or max_landmark_coordinates_fixed[2] > fixed_mask_np.shape[2]:
+            print('Landmarks (fixed) predicted outside image domain for patient {}'.format(pid))
+            error_found = True
+
+        if max_landmark_coordinates_moving[0] > moving_mask_np.shape[0] \
+                or max_landmark_coordinates_moving[1] > moving_mask_np.shape[1] \
+                or max_landmark_coordinates_moving[2] > moving_mask_np.shape[2]:
+            print('Landmarks (moving) predicted outside image domain for patient {}'.format(pid))
+            error_found = True
+
+        if error_found is True:
+            rows_to_delete = []
+            # Filter the landmark correspondences
+            fixed_image_landmarks_voxel_x = fixed_image_landmarks_voxel[:, 0]
+            fixed_image_landmarks_voxel_y = fixed_image_landmarks_voxel[:, 1]
+            fixed_image_landmarks_voxel_z = fixed_image_landmarks_voxel[:, 2]
+
+            moving_image_landmarks_voxel_x = moving_image_landmarks_voxel[:, 0]
+            moving_image_landmarks_voxel_y = moving_image_landmarks_voxel[:, 1]
+            moving_image_landmarks_voxel_z = moving_image_landmarks_voxel[:, 2]
+
+            fixed_rows_to_delete_x = np.where(fixed_image_landmarks_voxel_x >= fixed_mask_np.shape[0])
+            fixed_rows_to_delete_y = np.where(fixed_image_landmarks_voxel_y >= fixed_mask_np.shape[1])
+            fixed_rows_to_delete_z = np.where(fixed_image_landmarks_voxel_z >= fixed_mask_np.shape[2])
+
+            moving_rows_to_delete_x = np.where(moving_image_landmarks_voxel_x >= moving_mask_np.shape[0])
+            moving_rows_to_delete_y = np.where(moving_image_landmarks_voxel_y >= moving_mask_np.shape[1])
+            moving_rows_to_delete_z = np.where(moving_image_landmarks_voxel_z >= moving_mask_np.shape[2])
+
+            if len(fixed_rows_to_delete_x) != 0:
+                rows_to_delete.extend(list(fixed_rows_to_delete_x[0]))
+
+            if len(fixed_rows_to_delete_y) != 0:
+                rows_to_delete.extend(list(fixed_rows_to_delete_y[0]))
+
+            if len(fixed_rows_to_delete_z) != 0:
+                rows_to_delete.extend(list(fixed_rows_to_delete_z[0]))
+
+            if len(moving_rows_to_delete_x) != 0:
+                rows_to_delete.extend(list(moving_rows_to_delete_x[0]))
+
+            if len(moving_rows_to_delete_y) != 0:
+                rows_to_delete.extend(list(moving_rows_to_delete_y[0]))
+
+            if len(moving_rows_to_delete_z) != 0:
+                rows_to_delete.extend(list(moving_rows_to_delete_z[0]))
+
+            rows_to_delete = list(set(rows_to_delete)) # Get rid of duplicate entries
+            rows_to_delete = np.array(rows_to_delete)
+
+            fixed_image_landmarks_voxel = np.delete(fixed_image_landmarks_voxel,
+                                                    rows_to_delete,
+                                                    axis=0)
+
+            moving_image_landmarks_voxel = np.delete(moving_image_landmarks_voxel,
+                                                     rows_to_delete,
+                                                     axis=0)
 
         # 4. Count number of landmarks inside the respective lung masks
         fixed_mask_idxs = fixed_mask_np[fixed_image_landmarks_voxel[:, 0],
