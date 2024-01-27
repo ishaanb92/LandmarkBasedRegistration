21b89a6919ed4779fef7259541d07199cdb5edb0
kilgore92 <ishaan.bits92@gmail.com>
2024-01-27 13:25:06 +0100

    Plot histograms of lesion center distances

---
+++ analysis/get_distance_between_lesion_centers.py
@@ -0,0 +1,105 @@
+"""
+
+Quick and dirty script to get distance between lesion centers (after resampling the moving lesion mask)
+
+@author: Ishaan Bhat
+@email: ishaan@isi.uu.nl
+
+"""
+import os
+import numpy as np
+import joblib
+from argparse import ArgumentParser
+import json
+from lesionmatching.analysis.lesion_correspondence import *
+import pandas as pd
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('--reg_dir', type=str, required=True)
+    parser.add_argument('--gt_dir', type=str, required=True)
+
+    args = parser.parse_args()
+
+    pat_dirs = [f.path for f in os.scandir(args.reg_dir) if f.is_dir()]
+
+    tp_distances = []
+    fp_distances = []
+    fn_distances = []
+
+    for pat_dir in pat_dirs:
+
+        pid = pat_dir.split(os.sep)[-1]
+
+        # Load graph
+        try:
+            dgraph = joblib.load(os.path.join(pat_dir, 'corr_graph.pkl'))
+        except:
+            raise FileNotFoundError
+
+        # Load GT matches
+        with open(os.path.join(args.gt_dir,
+                               pid,
+                               'lesion_links.json')) as f:
+            gt_matches = json.load(f)
+
+        gt_dict = preprocess_gt_dict(gt_matches)
+
+        gt_forward_dict = gt_dict['forward_dict']
+
+        # Loop over edges and report distanes
+        for u, v in dgraph.edges():
+            if 'moving' in u.get_name().lower():
+                moving_lesion_id = u.get_idx()
+                fixed_lesion_id = v.get_idx()
+                moving_lesion_name = u.get_name()
+                fixed_lesion_name = v.get_name()
+            elif 'fixed' in u.get_name().lower():
+                fixed_lesion_id = u.get_idx()
+                moving_lesion_id = v.get_idx()
+                moving_lesion_name = v.get_name()
+                fixed_lesion_name = u.get_name()
+
+            prediction = dgraph.get_edge_data(u, v)['weight']
+            lesion_center_distance = dgraph.get_edge_data(u, v)['distance']
+
+            if prediction == 1:
+                # Check GT
+                if moving_lesion_name in gt_forward_dict[fixed_lesion_name]: # True positive
+                    tp_distances.append(lesion_center_distance)
+                else:
+                    fp_distances.append(lesion_center_distance)
+            else: # Match not predicted
+                if moving_lesion_name in gt_forward_dict[fixed_lesion_name]: # False negative
+                    fn_distances.append(lesion_center_distance)
+
+
+    tp_distances = np.array(tp_distances).astype(np.float32)
+    fp_distances = np.array(fp_distances).astype(np.float32)
+    fn_distances = np.array(fn_distances).astype(np.float32)
+
+    # Plot histogram
+    fig, ax = plt.subplots()
+    bins = np.linspace(0, 50, 100)
+    ax.hist(tp_distances, bins=bins, label='True Positives', alpha=0.8, edgecolor='black')
+    ax.hist(fp_distances, bins=bins, label='False Positives', alpha=0.8, edgecolor='black')
+    ax.hist(fn_distances, bins=bins, label='False Negatives', alpha=0.8, edgecolor='black')
+    ax.set_xlabel('Distance between lesion centers (mm)')
+    ax.set_ylabel('Count')
+    ax.vlines(x=[10],
+              ymin=0,
+              ymax=1,
+              colors='r',
+              linestyles='dashed',
+              transform=ax.get_xaxis_transform())
+
+    ax.legend(loc='upper right')
+
+    fig.savefig(os.path.join(args.reg_dir,
+                             'lesion_centers.pdf'),
+                bbox_inches='tight')
+
+    fig.savefig(os.path.join(args.reg_dir,
+                             'lesion_centers.jpg'),
+                bbox_inches='tight')
